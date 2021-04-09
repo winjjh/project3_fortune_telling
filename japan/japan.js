3d00c2d7e31d243829571ba2e2f47c6947f4f144
JongHee <jungj566@newschool.edu>
2021-04-09 21:50:55 +0900

    update in js

--- japan/japan.js
+++ japan/japan.js
@@ -34,3 +34,72 @@ document.addEventListener('DOMContentLoaded', function(){
          }
       }
 })
+
+
+// get my collection data, select all the records
+// specify functions that will recieve the data (in a row)
+base ("fortune_telling").select({}).eachPage(gotPageOfImage, gotAllImages);
+
+// an empty array to hold our data
+var Images = [];
+
+// callback function that recieve our data
+function gotPageOfImage(records, fetchNextPage) {
+  console.log("gotPageOfImage()");
+  // add the records from this page to our array
+  images.push(...records);
+  // request more pages
+  fetchNextPage();
+}
+
+// call back function that is called when all pages are loaded
+function gotAllImages(err) {
+  console.log("gotAllImages()");
+
+  // report an error, you'd want to do something better than this in production
+  if (err) {
+    console.log("error loading data");
+    console.error(err);
+    return;
+  }
+
+// call functions to log and show the books
+consoleLogImages();
+showImages();
+}
+
+// just loop through thour air tablee data and console.log it
+function consoleLogImages() {
+  console.log("consoleLogImages()");
+  images.forEach((image) => {
+    console.log("image:", image);
+  });
+}
+
+// look through our airtable data, create elements
+function showImages() {
+  console.log("showImages()");
+  images.forEach((japan) => {
+    
+    var countryCategory = image.fields.country_category
+    countryCategory.forEach((category) => {
+      
+      if (category == 'japan') {
+        
+        // display japan images
+        var japanImages = document.createElement("img");
+        japanImages.classList.add('japan-image');
+        japanImages.srrc = japan.fields.img[0].url;
+        document.querySelector.apply(".page").appendChild(japanImages);
+      }
+
+      // add description to our song container
+      var japanDescription = document.createElement("p");
+      japanDescription.classList.add("japan-description");
+      japanDescription.innerText = japan.fields.description;
+      songContainer.append(japanDescription);
+
+    })
+  });
+}
+
